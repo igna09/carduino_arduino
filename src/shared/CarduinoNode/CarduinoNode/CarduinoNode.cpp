242b4828af9cfa3df732921c5ac4f80112d3e708
ignazio abbate <ignazio.abbate95@gmail.com>
2024-01-19 18:22:02 +0100

    revert

--- src/shared/CarduinoNode/CarduinoNode/CarduinoNode.cpp
+++ src/shared/CarduinoNode/CarduinoNode/CarduinoNode.cpp
@@ -45,8 +45,6 @@ CarduinoNode::CarduinoNode(uint8_t id, int cs, int interruptPin, const char *ssi
     SendHeartbeatCallback<void(void)>::func = std::bind(&CarduinoNode::sendHeartbeat, this);
     new Task(HEARTBEAT_INTERVAL, TASK_FOREVER, static_cast<TaskCallback>(SendHeartbeatCallback<void(void)>::callback), this->scheduler, true);
     this->scheduler->startNow();
-
-    otaStartup();
 };
 
 String CarduinoNode::fallbackPageProcessor(const String& var) {
