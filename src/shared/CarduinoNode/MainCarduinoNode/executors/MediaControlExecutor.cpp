26892cf2e1aa648802119696cd6df880618a7bc4
ignazio abbate <ignazio.abbate95@gmail.com>
2024-04-27 10:51:44 +0200

    fix error

--- src/shared/CarduinoNode/MainCarduinoNode/executors/MediaControlExecutor.cpp
+++ src/shared/CarduinoNode/MainCarduinoNode/executors/MediaControlExecutor.cpp
@@ -3,11 +3,11 @@
 MediaControlExecutor::MediaControlExecutor() : CarduinoNodeExecutorInterface(&Category::MEDIA_CONTROL) {};
 
 void MediaControlExecutor::execute(CarduinoNode *node, CanbusMessage *message){
-    MediaControlMessage mediaControlMessage = MediaControlMessage((MediaControl*)MediaControl::getValueById(message->messageId));
-
-    if(mediaControlMessage.id == MediaControl::LONG_PRESS.id) {
+    MediaControl *mediaControl = (MediaControl*)MediaControl::getValueById(message->messageId);
+    if(mediaControl->id == MediaControl::LONG_PRESS.id) {
+        MediaControlMessage mediaControlMessage = MediaControlMessage(mediaControl);
         ((MainCarduinoNode*)node)->sendSerialMessage(&mediaControlMessage);
-    } else if(mediaControlMessage.id != MediaControl::LONG_PRESS.id) { // TODO: move to new node canbus executor
-        ((MainCarduinoNode*)node)->executeSwcCommand((MediaControl*)mediaControlMessage.mediaControl);
+    } else if(mediaControl->id != MediaControl::LONG_PRESS.id) { // TODO: move to new node canbus executor
+        ((MainCarduinoNode*)node)->executeSwcCommand(mediaControl);
     }
 };
