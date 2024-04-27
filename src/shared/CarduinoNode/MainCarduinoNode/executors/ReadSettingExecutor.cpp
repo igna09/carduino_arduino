65432a63d675fecc5c5070401158429b1e641ff3
ignazio abbate <ignazio.abbate95@gmail.com>
2024-04-27 11:17:59 +0200

    fix read setting swc binding

--- src/shared/CarduinoNode/MainCarduinoNode/executors/ReadSettingExecutor.cpp
+++ src/shared/CarduinoNode/MainCarduinoNode/executors/ReadSettingExecutor.cpp
@@ -11,4 +11,11 @@ void ReadSettingExecutor::execute(CarduinoNode *node, CanbusMessage *message) {
     TypedCanbusMessage resetTypedCanbusMessage = TypedCanbusMessage(node->generateId(Category::READ_SETTINGS, Setting::RESTART), false);
     SettingMessage resetSettingMessage(resetTypedCanbusMessage);
     ((MainCarduinoNode*)node)->sendSerialMessage(&resetSettingMessage);
+
+    /**
+     * TODO: move to new node, should add method to send read setting on canbus?
+    */
+    TypedCanbusMessage swcBindingTypedCanbusMessage = TypedCanbusMessage(node->generateId(Category::READ_SETTINGS, Setting::SWC_PAIR), false);
+    SettingMessage swcBindingSettingMessage(swcBindingTypedCanbusMessage);
+    ((MainCarduinoNode*)node)->sendSerialMessage(&swcBindingSettingMessage);
 };
