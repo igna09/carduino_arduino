3c52c40dd2c9b41d703dd59ea820f44b51709497
ignazio abbate <ignazio.abbate95@gmail.com>
2024-05-04 10:55:02 +0200

    using new interface

--- src/shared/CarduinoNode/CarduinoNode/executors/CarduinoNodeCanGetHellos/CarduinoNodeCanGetHellos.cpp
+++ src/shared/CarduinoNode/CarduinoNode/executors/CarduinoNodeCanGetHellos/CarduinoNodeCanGetHellos.cpp
@@ -1,17 +1,12 @@
 #include "CarduinoNodeCanGetHellos.h"
 
-CarduinoNodeCanGetHellos::CarduinoNodeCanGetHellos() : CarduinoNodeExecutorInterface(&Category::EVENT) {};
+CarduinoNodeCanGetHellos::CarduinoNodeCanGetHellos() : CarduinoNodeExecutorInterface(&Category::EVENT, Event::GET_HELLOS.id) {};
 
 void CarduinoNodeCanGetHellos::execute(CarduinoNode *node, CanbusMessage *message) {
     EventMessage *eventMessage = new EventMessage(message);
-    
-    if(eventMessage->id == Event::GET_HELLOS.id) {
-        EventMessage *helloMessage = new EventMessage(&Event::HELLO, node->id);
-
-        node->sendCanbusMessage(helloMessage);
-
-        delete helloMessage;
-    }
-
     delete eventMessage;
+
+    EventMessage *helloMessage = new EventMessage(&Event::HELLO, node->id);
+    node->sendCanbusMessage(helloMessage);
+    delete helloMessage;
 };
