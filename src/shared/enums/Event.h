1c87d5aa80504ee54b739861aea2a472e0ac702a
ignazio abbate <ignazio.abbate95@gmail.com>
2024-05-04 12:06:27 +0200

    fix

--- src/shared/enums/Event.h
+++ src/shared/enums/Event.h
@@ -68,5 +68,5 @@ inline const Event Event::TURN_OFF = Event(0x03, "TURN_OFF", &CanbusMessageType:
 inline const Event Event::TURN_OFF_INTERRUPT = Event(0x04, "TURN_OFF_INTERRUPT", &CanbusMessageType::INT);
 inline const Event Event::TURN_OFF_FINISH = Event(0x05, "TURN_OFF_FINISH", &CanbusMessageType::INT);
 inline const Event Event::HELLO = Event(0x06, "HELLO", &CanbusMessageType::INT); //contains id sending hello (heartbeat and events used to get a list of nodes)
-inline const Event Event::GET_HELLOS = Event(0x07, "GET_HELLOS", &CanbusMessageType::BOOL); //request all hellos messages
+inline const Event Event::GET_HELLOS = Event(0x07, "GET_HELLOS", &CanbusMessageType::INT); //request all hellos messages
 inline const Event Event::HEARTBEAT = Event(0x08, "HEARTBEAT", &CanbusMessageType::INT);
