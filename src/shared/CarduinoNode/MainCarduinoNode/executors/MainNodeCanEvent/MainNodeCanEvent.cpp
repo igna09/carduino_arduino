45dde28d56659e00a7fbb0f9676cd2dc22884eae
ignazio abbate <i.abbate@reactive-almaviva.it>
2024-05-27 12:38:26 +0200

    removed prints

--- src/shared/CarduinoNode/MainCarduinoNode/executors/MainNodeCanEvent/MainNodeCanEvent.cpp
+++ src/shared/CarduinoNode/MainCarduinoNode/executors/MainNodeCanEvent/MainNodeCanEvent.cpp
@@ -7,16 +7,11 @@ void MainNodeCanEvent::execute(CarduinoNode *node, CanbusMessage *message) {
     MainCarduinoNode *mainCarduinoNode = (MainCarduinoNode*)node;
     
     if(eventMessage->event->id == Event::HELLO.id) {
-        Serial.println("here 0");
         NodeInformation *nodeInformation = mainCarduinoNode->createOrGetNodeInformation(eventMessage->senderReceiverId);
-        Serial.println("here -a");
-        Serial.println(nodeInformation == nullptr);
 
         nodeInformation->lastTimeReceivedHeartBeat = millis();
-        Serial.println("here -c");
         nodeInformation->lastCompletedEvent = nullptr;
 
-        Serial.println("here -b");
         node->sendEvent(&Event::TURN_ON, nodeInformation->id);
     } else if(eventMessage->event->id == Event::TURN_OFF_FINISH.id || eventMessage->event->id == Event::TURN_ON_FINISH.id) {
         NodeInformation *nodeInformation = mainCarduinoNode->getNodeInformation(eventMessage->senderReceiverId);
