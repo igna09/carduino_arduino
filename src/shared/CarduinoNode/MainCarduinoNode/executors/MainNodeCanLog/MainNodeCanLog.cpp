b44ed8f8a9057c892c8fcbb922d8b42780edff98
ignazio abbate <i.abbate@reactive-almaviva.it>
2024-06-17 12:38:15 +0200

    reverted isEnabled

--- src/shared/CarduinoNode/MainCarduinoNode/executors/MainNodeCanLog/MainNodeCanLog.cpp
+++ src/shared/CarduinoNode/MainCarduinoNode/executors/MainNodeCanLog/MainNodeCanLog.cpp
@@ -3,12 +3,10 @@
 MainNodeCanLog::MainNodeCanLog() : CarduinoNodeExecutorInterface(&Category::LOG) {};
 
 void MainNodeCanLog::execute(CarduinoNode *node, CanbusMessage *message) {
-    if(node->isEnabled) {
-        LogMessage *logMessage = new LogMessage(message);
-        MainCarduinoNode *mainCarduinoNode = (MainCarduinoNode*)node;
-        
-        mainCarduinoNode->sendSerialMessage(logMessage);
+    LogMessage *logMessage = new LogMessage(message);
+    MainCarduinoNode *mainCarduinoNode = (MainCarduinoNode*)node;
+    
+    mainCarduinoNode->sendSerialMessage(logMessage);
 
-        delete logMessage;
-    }
+    delete logMessage;
 };
