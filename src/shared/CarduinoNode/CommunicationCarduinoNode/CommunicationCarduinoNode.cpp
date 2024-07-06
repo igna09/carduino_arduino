3790adea3dfe3e1f3f73ac17810f6ff5c3e2b14c
ignazio abbate <ignazio.abbate95@gmail.com>
2024-07-06 11:14:00 +0200

    log

--- src/shared/CarduinoNode/CommunicationCarduinoNode/CommunicationCarduinoNode.cpp
+++ src/shared/CarduinoNode/CommunicationCarduinoNode/CommunicationCarduinoNode.cpp
@@ -38,7 +38,7 @@ CommunicationCarduinoNode::CommunicationCarduinoNode(uint8_t id, int cs, int int
         int8_t rssi;
         NimBLEConnInfo info = bleServer->getPeerInfo(0);
         ble_gap_conn_rssi(info.getConnHandle(), &rssi);
-        String message = "authenticated [bd_addr: ";
+        String message = "rssi [bd_addr: ";
         message += info.getIdAddress().toString().c_str();
         message += ", RSSI: ";
         message += String(rssi);
