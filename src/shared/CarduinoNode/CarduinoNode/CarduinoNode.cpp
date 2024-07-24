d903b21c5390dddd7fbdb0c5d6f788b35a02adc1
ignazio abbate <ignazio.abbate95@gmail.com>
2024-07-24 20:26:28 +0200

    restore settings backup

--- src/shared/CarduinoNode/CarduinoNode/CarduinoNode.cpp
+++ src/shared/CarduinoNode/CarduinoNode/CarduinoNode.cpp
@@ -76,7 +76,7 @@ CarduinoNode::CarduinoNode(uint8_t id, int cs, int interruptPin, const char *ssi
         this->backupSettings();
         this->printlnWrapper("CarduinoNode::CarduinoNode LAMBDA settings saved");
     }, this->scheduler);
-    // backupSettingsTask->restartDelayed();
+    backupSettingsTask->restartDelayed();
     
     if(!this->initializedCan) {
         this->otaStartup();
