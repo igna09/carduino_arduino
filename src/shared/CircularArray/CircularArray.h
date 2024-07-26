7f7ae9547244fd5a0723f8cc9b4d4279589d0612
ignazio abbate <ignazio.abbate95@gmail.com>
2024-07-26 12:07:52 +0200

    creata classe circular array

---
+++ src/shared/CircularArray/CircularArray.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <Arduino.h>
+
+template <typename T, size_t N>
+class CircularArray {
+  public:
+    CircularArray() : head(0), tail(0) {}
+
+    void push(const T& value) {
+      array[head] = value;
+      head = (head + 1) % N;
+      if (head == tail) {
+        tail = (tail + 1) % N;
+      }
+    }
+
+    T pop() {
+      if (head == tail) {
+        return T(); // Return default value if empty
+      }
+      T value = array[tail];
+      tail = (tail + 1) % N;
+      return value;
+    }
+
+    bool isEmpty() const {
+      return head == tail;
+    }
+
+    bool isFull() const {
+      return (head + 1) % N == tail;
+    }
+
+    size_t size() const {
+      return (head - tail + N) % N;
+    }
+
+    T& operator[](size_t index) {
+      return array[(tail + index) % N];
+    }
+
+    const T& operator[](size_t index) const {
+      return array[(tail + index) % N];
+    }
+
+  private:
+    T array[N];
+    size_t head;
+    size_t tail;
+};
