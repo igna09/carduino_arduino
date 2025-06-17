2223caa7647a0dca38e5dd714b2618bebba9808c
ABBATE IGNAZIO <ignazio.abbate@esternibisp.com>
2025-06-17 17:46:40 +0200

    added digipot

--- src/shared/CarduinoNode/MediaControlCarduinoNode/MediaControlCarduinoNode.cpp
+++ src/shared/CarduinoNode/MediaControlCarduinoNode/MediaControlCarduinoNode.cpp
@@ -1,23 +1,36 @@
 #include "MediaControlCarduinoNode.h"
 
-MediaControlCarduinoNode::MediaControlCarduinoNode(uint8_t id, uint8_t clk, uint8_t dt, uint8_t sw, int cs, int interruptPin, const char *ssid, const char *password) : CarduinoNode(id, cs, interruptPin, ssid,  password, false, true, true) {
+/**
+ * TODO: 
+ * 	taskscheduler che riporta la resistenza a 0 dopo tot di ms (da recuperare da main node)
+ * 	portare la gestione del mapping dei pulsanti da main node a qui
+ * 	durante la registrazione dei pulsanti mandare messaggio alla radio (non visibile perché siamo su app radio swc)
+ * 	aggiungere buzzer
+ * 	far suonare buzzer durante il cambio pulsanti registrazione
+ * 	aggiungere mapping MediaControl => resistance value
+ * 	far partire registrazione dei pulsanti solo quando viene premuto il pulsante
+ */
+
+MediaControlCarduinoNode::MediaControlCarduinoNode(uint8_t id, uint8_t clk, uint8_t dt, uint8_t sw, int cs, int interruptPin, uint8_t digiPotCs, uint8_t digiPotUd, uint8_t digiPotInc, const char *ssid, const char *password) : CarduinoNode(id, cs, interruptPin, ssid,  password, false, true, true) {
     this->restoreSettings();
 	
 	versatileEncoder = new Versatile_RotaryEncoder(clk, dt, sw);
-
 	this->lastRead = 0;
-
 	versatileEncoder->setHandleRotate([this](uint8_t rotation){
 		if(!this->canRead()) {
 			return;
 		}
 		this->lastRead = millis();
 		if(rotation == 255) { // clockwise
-			this->sendMediaControlMessage(&MediaControl::VOLUME_UP);
+			// this->sendMediaControlMessage(&MediaControl::VOLUME_UP);
 			// Serial.println("VOLUME_UP");
+			this->x9c103s->setResistance(100);
+			this->releaseButtonDelayed();
 		} else if (rotation == 1) { //counter clockwise
-			this->sendMediaControlMessage(&MediaControl::VOLUME_DOWN);
+			// this->sendMediaControlMessage(&MediaControl::VOLUME_DOWN);
 			// Serial.println("VOLUME_DOWN");
+			this->x9c103s->setResistance(80);
+			this->releaseButtonDelayed();
 		}
 	});
 	versatileEncoder->setHandlePressRelease([this](){
@@ -25,7 +38,18 @@ MediaControlCarduinoNode::MediaControlCarduinoNode(uint8_t id, uint8_t clk, uint
 			return;
 		}
 		this->lastRead = millis();
-		this->sendMediaControlMessage(&MediaControl::PLAY_PAUSE);
+		// this->sendMediaControlMessage(&MediaControl::PLAY_PAUSE);
+		this->x9c103s->setResistance(60);
+		this->releaseButtonDelayed();
+	});
+	versatileEncoder->setHandleDoublePressRelease([this](){
+		if(!this->canRead()) {
+			return;
+		}
+		this->lastRead = millis();
+		// this->sendMediaControlMessage(&MediaControl::NEXT);
+		this->x9c103s->setResistance(40);
+		this->releaseButtonDelayed();
 	});
 	versatileEncoder->setHandleLongPress([this](){
 		if(!this->canRead()) {
@@ -34,12 +58,18 @@ MediaControlCarduinoNode::MediaControlCarduinoNode(uint8_t id, uint8_t clk, uint
 		this->lastRead = millis();
 		this->sendMediaControlMessage(&MediaControl::LONG_PRESS);
 	});
-	versatileEncoder->setHandleDoublePressRelease([this](){
-		if(!this->canRead()) {
-			return;
-		}
-		this->lastRead = millis();
-		this->sendMediaControlMessage(&MediaControl::NEXT);
+
+	x9c103s = new X9C103S(digiPotInc, digiPotUd, digiPotInc);
+	x9c103s->initializePot();
+};
+
+void MediaControlCarduinoNode::releaseButtonDelayed() {
+	if(noButtonTask->isEnabled()) {
+		noButtonTask->cancel();
+	}
+
+	noButtonTask = this->delayTask(SWC_PRESS_INTERVAL, [&](){
+		this->x9c103s->setResistance(0);
 	});
 };
 
--- src/shared/CarduinoNode/MediaControlCarduinoNode/MediaControlCarduinoNode.h
+++ src/shared/CarduinoNode/MediaControlCarduinoNode/MediaControlCarduinoNode.h
@@ -6,18 +6,28 @@
 #include "shared/enums/MediaControl.h"
 #include "shared/CanbusMessage/MediaControlMessage/MediaControlMessage.h"
 #include "shared/SharedDefinitions.h"
+#include "X9C103S.h"
+
+#define SWC_PRESS_INTERVAL  65
+#define SWC_PAIRING_INTERVAL  5000
+#define SWC_WAITING_PAIRING_INTERVAL  1000
+#define SWC_FIRST_WAITING_PAIRING_INTERVAL  5000
+#define SWC_PIN_SIZE 8
 
 // #define ENCODER_READING_INTERVAL 75
 #define ENCODER_READING_INTERVAL 0
 
 class MediaControlCarduinoNode : public CarduinoNode {
     public:
-        MediaControlCarduinoNode(uint8_t id, uint8_t clk, uint8_t dt, uint8_t sw, int cs, int interruptPin, const char *ssid, const char *password);
+        MediaControlCarduinoNode(uint8_t id, uint8_t clk, uint8_t dt, uint8_t sw, int cs, int interruptPin, uint8_t digiPotCs, uint8_t digiPotUd, uint8_t digiPotInc, const char *ssid, const char *password);
         void loop();
         void sendMediaControlMessage(const MediaControl*);
+        void releaseButtonDelayed();
 
     private:
         Versatile_RotaryEncoder *versatileEncoder;
+        X9C103S *x9c103s;
+        Task *noButtonTask;
         unsigned long lastRead;
         bool canRead();
 };
